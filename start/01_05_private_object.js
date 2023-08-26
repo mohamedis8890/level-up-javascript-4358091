439895f179195c4186d63a1e3ee3baaf92ff05f8
Mo Saleh <mohamed.is8890@gmail.com>
2023-08-26 22:59:39 +0000

    Level 5

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,15 @@
 // Write your code here
+
+class Person {
+  constructor(name, username, password) {
+    const uname = Symbol("username");
+    const pass = Symbol("password");
+    this.name = name;
+    this[uname] = username;
+    this[pass] = password;
+  }
+}
+
+const mo = new Person("Mo", "mosaleh", "p455w0rd");
+
+console.log(Object.keys(mo));
