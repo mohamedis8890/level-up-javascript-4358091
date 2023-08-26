d5f1f83f190a6cb62130e6cbc4d9fbaaea735c2b
Mo Saleh <mohamed.is8890@gmail.com>
2023-08-26 23:24:17 +0000

    Level 7

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,20 @@
 // Write your code here
+function getStudents(classroom) {
+  const { hasTeachingAssisstant, classes } = classroom;
+  let teacher, assistant, students;
+
+  if (hasTeachingAssisstant) {
+    [teacher, assistant, ...students] = classes;
+  } else {
+    [teacher, ...students] = classes;
+  }
+
+  return students;
+}
+
+const students = getStudents({
+  hasTeachingAssisstant: false,
+  classes: ["c1", "c2", "c3", "c4", "c5"],
+});
+
+console.log(students);
