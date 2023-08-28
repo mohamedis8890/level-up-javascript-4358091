17f330e663e62708862ccbdecd5c38e9c2164df2
mohamedis8890 <mohamed.is8890@gmail.com>
2023-08-28 22:48:38 +0300

    Level 9

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+function totalBill(drinks = [], price = 1) {
+  const total = drinks.reduce((runningTotal, drink) => {
+    return runningTotal + drink * price;
+  });
+
+  return `The total bill is $${total}`;
+}
+
+const drinks = [2, 3, 1, 5];
+const price = 1.25;
+console.log(totalBill(drinks, price));
