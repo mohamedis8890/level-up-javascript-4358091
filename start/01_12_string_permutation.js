59f0f23aa2caaec8bb8a0d74291b115decae5ec6
mohamedis8890 <mohamed.is8890@gmail.com>
2023-08-29 00:27:07 +0300

    Level 12

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,9 @@
 // Write your code here
+
+function permutations(word) {
+  if (word.length === 1) return 1;
+
+  return permutations(word.slice(0, -1)) * word.length;
+}
+
+console.log(permutations("hey"));
